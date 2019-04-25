16d26a3a5041b18345601bfccc18b8d9cd2c00fc
Peter Pinch <pdpinch+github@hawklake.com>
2019-04-24 21:50:26 -0400

    commas!

--- bootcamps.js
+++ bootcamps.js
@@ -10,7 +10,7 @@
 	"application_starts": "",
 	"application_ends" : "04/15/2019",
 	"instructors": [ "", "" ],
-	"image": "http://placehold.it/940x300/999/CCC"
+	"image": "http://placehold.it/940x300/999/CCC",
 	"topics": ["Business & Management", " Entrepreneurship", "Innovation"]
 },{
 	"title": "MIT HMS Healthcare Innovation Bootcamp",
@@ -22,7 +22,7 @@
 	"enrollment_starts": "",
 	"enrollment_ends" : "",
 	"instructors": [ "", "" ],
-	"image": "http://placehold.it/940x300/999/CCC"
+	"image": "http://placehold.it/940x300/999/CCC",
 	"topics": ["Business & Management", " Entrepreneurship", "Innovation"] 
 },{
 	"title": "MIT HMS Healthcare Innovation Bootcamp",
@@ -34,7 +34,7 @@
 	"enrollment_starts": "",
 	"enrollment_ends" : "",
 	"instructors": [ "", "" ],
-	"image": "http://placehold.it/940x300/999/CCC"
+	"image": "http://placehold.it/940x300/999/CCC",
 	"topics": ["Business & Management", " Entrepreneurship", "Innovation"] 
 }
 ]
