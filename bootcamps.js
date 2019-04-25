e5f665b057cecbfbdf346cb21eb5cf22bcf6f49a
Peter Pinch <pdpinch+github@hawklake.com>
2019-04-24 21:31:46 -0400

    replace newlines with HTML formatting

--- bootcamps.js
+++ bootcamps.js
@@ -2,18 +2,7 @@
 	"title": "MIT HMS Healthcare Innovation Bootcamp",
 	"marketing_url": "https://bootcamp.mit.edu/entrepreneurship/healthcare-innovation/",
 	"short_description": "The MIT-Harvard Medical School Healthcare Innovation Bootcamp",
-	"full_description": "The MIT-HMS Healthcare Innovation Bootcamp is a new accelerated-learning program by \
-	                     Harvard Medical School’s Center for Primary Care and the MIT Bootcamps. \
-	
-			     You will attend lectures on healthcare, innovation, and entrepreneurship from \
-			     faculty at both universities, and work on project teams to start a new healthcare \
-			     venture in one week.\
-	
-			     You will come out the other end armed with a comprehensive toolkit for healthcare innovation.\
-			     What’s more, you will join a global community of innovators passionate about solving \
-			     healthcare related problems and other innovation challenges. \
-			     In one week, the Bootcamp will provide you with experiences healthcare entrepreneurs \
-			     face in the first 1-2 years of a new venture.",
+	"full_description": "<p>The MIT-HMS Healthcare Innovation Bootcamp is a new accelerated-learning program by Harvard Medical School’s Center for Primary Care and the MIT Bootcamps.</p><p>You will attend lectures on healthcare, innovation, and entrepreneurship from faculty at both universities, and work on project teams to start a new healthcare venture in one week.</p><p>You will come out the other end armed with a comprehensive toolkit for healthcare innovation.</p><p>What’s more, you will join a global community of innovators passionate about solving healthcare related problems and other innovation challenges. In one week, the Bootcamp will provide you with experiences healthcare entrepreneurs face in the first 1-2 years of a new venture.</p>",
 	"location: Cambridge, MA",
 	"price": "8500",
 	"start_date": "06/15/2019",
